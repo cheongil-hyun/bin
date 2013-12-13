f9ce9390e82de0d4d9110200880be5545eb7f822
cheongil hyun <blkhyun@gmail.com>
2013-12-13 23:35:32 +0900

    add the install-package.sh for ubuntu desktop

---
+++ install-package.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# install package for the new install 
+
+sudo apt-get install -y vim
+sudo apt-get install -y emacs
+sudo apt-get install -y byobu
+sudo apt-get install -y git
+sudo apt-get install -y ctags
+sudo apt-get install -y cscope
+sudo apt-get install -y comix
+sudo apt-get install -y manpages-dev manpages-posix manpages-posix-dev
+sudo apt-get install -y make automake autoconf
+sudo apt-get install -y vlc
+sudo apt-get install -y curl
+sudo apt-get install -y dosbox
+sudo apt-get install -y xbmc
+sudo apt-get install -y chromium-browser
+#sudo apt-get install -y virtualbox
+#sudo add-apt-repository ppa:webupd8team/java
+#sudo apt-get update
+#sudo apt-get install oracle-jdk7-installer
+#sudo apt-get install eclipse
+#cp /usr/lib/jni/libswt-* ~/.swt/lib/linux/x86_64/
