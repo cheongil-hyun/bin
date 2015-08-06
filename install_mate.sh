24e3513a28fe1e4d07272a446b337d81e59644eb
cheongil.hyun <cheongil.hyun@lge.com>
2015-08-06 11:00:48 +0900

    script: install_mate.sh: install mate environment

---
+++ install_mate.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo add-apt-repository "deb http://repo.mate-desktop.org/archive/1.8/ubuntu trusty main"
+sudo apt-get update
+sudo apt-get install mate-core
+sudo apt-get install mate-desktop-environment
+sudo apt-get install mate-desktop-environment-extra
